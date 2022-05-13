8f364b3eeb4090fe0a33a221f0fd06aaf7fd0e67
Shmuel Weinfeld <shmuelweinfeld@gmail.com>
2022-05-12 23:15:45 -0400

    added inspiration route

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
-
+const fs = require('fs');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8000
-
 
 //Middleware
 app.use(cors())
 app.use(express.json());
 
+// Function to read videos
+function readImages() {
+  const imagesFile = fs.readFileSync('./data/images.json');
+  const imagesData = JSON.parse(imagesFile);
+  return imagesData;
+}
 
-app.get("/hello", (req, res) => {
-    res.send("Hello world, server test");
-  });
+app.get("/inspiration", (req, res) => {
+  // 1. Read the data (and parse it)
+  const imagesData = readImages();
+
+  // 3. Respond with that data
+  res.json(imagesData);
+
+});
 
 
 app.listen(PORT, ()=> {
